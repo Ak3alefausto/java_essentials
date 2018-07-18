3c9c0ec047e0e85796665bfc4fb0767a1874bcd4
Don Eduardo <Ak3alefausto@users.noreply.github.com>
2018-07-18 14:39:59 -0500

    Add files via upload

--- src/src1/tareas/Buggy0.java
+++ src/src1/tareas/Buggy0.java
@@ -1,10 +1,13 @@
 package src1.tareas;
+import java.io.BufferedInputStream;
+
+import libs.Input;
 
 public class Buggy0 {
 
 	public static void main(String[] args) {
-		//Input.print("hello, world\n");
-
+		Input.print("hello, world\n");
+		
 	}
 
 }
